33caf2e91e13551d30b1177fe6df57ee488792e0
Shih-Kuan Chen <77881912+ShihKuanChen@users.noreply.github.com>
2026-01-13 15:02:27 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,13 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class User(db.Modle):
+    id = db.Column(db.Integer, primary_key=True)
 
 @app.route("/<board>/<id>")
 def get_article(board, id):
@@ -11,4 +18,3 @@ def get_titles(board):
     limit = request.args.get('limit', default=30, type=int)
     start_id = request.args.get('start_id')
     board = request.args.get('board')
-
